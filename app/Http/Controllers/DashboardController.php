b11bc66d11cd5ba1b731db1209a7e7c6846f75d2
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2020-09-11 16:03:12 -0500

    how to request

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,10 +6,16 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-   public function index (){
+    // asi se hace un request
+   public function index (Request $request){
+       dd($request->query('title'));
         return view('dashboard', [
             //arreglo asociativo
             'title' => 'Curso laravel en platzi!!!!'
         ]);
     }
 }
+
+// php artisan serve = CREA UN SERVER LOCAL
+
+
