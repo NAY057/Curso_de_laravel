02ce77c090e4888026ab0363aa3c12ff1b0bf3dd
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2020-09-11 16:07:33 -0500

    how to request 2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,14 +8,14 @@ class DashboardController extends Controller
 {
     // asi se hace un request
    public function index (Request $request){
-       dd($request->query('title'));
         return view('dashboard', [
             //arreglo asociativo
-            'title' => 'Curso laravel en platzi!!!!'
+            'title' => $request->query('title', 'SRY =(')
         ]);
     }
 }
 
 // php artisan serve = CREA UN SERVER LOCAL
+// Contamos con un helper muy útil de Laravel que reemplaza el var_dump y el die; este helper es dd.
 
 
