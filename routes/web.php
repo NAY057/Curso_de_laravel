4c4ccb3caa6cd01c7a365e6a73de5dfe4344c6f8
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2020-09-13 02:31:20 -0500

    commands edited, nre files added

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 // });
 // Route::get('/dashboard',function(){
 //     return view('test', [
-//         //arreglo asociativo
+//         //arregl o asociativo
 //         'title' => 'Curso laravel en platzi!!!!'
 //     ]);
 // });
@@ -26,3 +26,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\HomeController@index');
 
 Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index');
+
+Route::resource('/expense_reports', 'App\Http\Controllers\ExpenseReportController');
+
